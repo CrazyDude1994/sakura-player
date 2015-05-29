7f44b9a8ba9bcadbef767322c7d33d718898f957
Kartavtsev Sergey <crazyserega1994@mail.ru>
2015-05-29 08:50:04 +0300

    Added listener to tutorialfragment

--- app/src/main/java/com/crazydude/sakuraplayer/gui/fragments/LastfmTutorialFragment.java
+++ app/src/main/java/com/crazydude/sakuraplayer/gui/fragments/LastfmTutorialFragment.java
@@ -1,5 +1,7 @@
 package com.crazydude.sakuraplayer.gui.fragments;
 
+import android.support.v4.view.ViewPager;
+
 import com.crazydude.sakuraplayer.R;
 import com.crazydude.sakuraplayer.views.fragments.LastfmTutorialFragmentView;
 
@@ -11,13 +13,32 @@ import org.androidannotations.annotations.EFragment;
  * Created by kartavtsev.s on 28.05.2015.
  */
 @EFragment(R.layout.fragment_lastfm_tutorial)
-public class LastfmTutorialFragment extends BaseFragment {
+public class LastfmTutorialFragment extends BaseFragment implements ViewPager.OnPageChangeListener {
 
     @Bean
     LastfmTutorialFragmentView mLastfmTutorialFragmentView;
 
     @AfterViews
     void initViews() {
+        mLastfmTutorialFragmentView.addOnPageChangeListener(this);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        if ((position - 1) == mLastfmTutorialFragmentView.getPagesCount()) {
+            mLastfmTutorialFragmentView.showLoginButton();
+        } else {
+            mLastfmTutorialFragmentView.hideLoginButton();
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
 
     }
 }
--- app/src/main/java/com/crazydude/sakuraplayer/views/fragments/LastfmTutorialFragmentView.java
+++ app/src/main/java/com/crazydude/sakuraplayer/views/fragments/LastfmTutorialFragmentView.java
@@ -1,6 +1,9 @@
 package com.crazydude.sakuraplayer.views.fragments;
 
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
 
 import com.andraskindler.parallaxviewpager.ParallaxViewPager;
 import com.crazydude.sakuraplayer.R;
@@ -24,6 +27,9 @@ public class LastfmTutorialFragmentView extends BaseFragmentView {
     @ViewById(R.id.fragment_lastfm_tutorial_indicator)
     CirclePageIndicator mPageIndicator;
 
+    @ViewById(R.id.fragment_lastfm_tutorial_button)
+    Button mTutorialButton;
+
     @RootContext
     FragmentActivity mContext;
 
@@ -36,5 +42,24 @@ public class LastfmTutorialFragmentView extends BaseFragmentView {
         mViewPager.setAdapter(mPagerAdapter);
         mPageIndicator.setViewPager(mViewPager);
         mViewPager.addOnPageChangeListener(mPageIndicator);
+    }
+
+    public void showLoginButton() {
+        mTutorialButton.setVisibility(View.VISIBLE);
+    }
+
+    public void hideLoginButton() {
+        mTutorialButton.setVisibility(View.GONE);
+    }
+
+    public void addOnPageChangeListener(ViewPager.OnPageChangeListener listener) {
+        mViewPager.addOnPageChangeListener(listener);
+    }
+
+    public int getPagesCount() {
+        if (mPagerAdapter != null) {
+            return mPagerAdapter.getCount();
+        }
+        return 0;
     }
 }
