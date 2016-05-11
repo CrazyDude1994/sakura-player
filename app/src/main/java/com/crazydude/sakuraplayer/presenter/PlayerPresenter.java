0908f4100ba395cb36825f27bc61a867d030f83e
Kartavtsev Sergey <crazyserega1994@mail.ru>
2016-05-11 18:44:55 +0300

    Added travis

---
+++ app/src/main/java/com/crazydude/sakuraplayer/presenter/PlayerPresenter.java
@@ -0,0 +1,7 @@
+package com.crazydude.sakuraplayer.presenter;
+
+/**
+ * Created by Crazy on 02.05.2016.
+ */
+public class PlayerPresenter {
+}
---
+++ app/src/main/java/com/crazydude/sakuraplayer/views/TracklistAllView.java
@@ -0,0 +1,7 @@
+package com.crazydude.sakuraplayer.views;
+
+/**
+ * Created by Crazy on 02.05.2016.
+ */
+public interface TracklistAllView {
+}
